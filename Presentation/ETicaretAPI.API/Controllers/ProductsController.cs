effba7a1b851239334f8653f3cd302277a52d253
qwe asd <aliklft61@outlook.com>
2022-03-06 22:44:11 +0300

    Proje dosyası ekle.

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductReadRepository _productReadRepository;
+        readonly private IProductWriteRepository _productWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository,IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+            _productWriteRepository.AddRangeAsync(new()
+            {
+                new() { Id = Guid.NewGuid(), Name = "Product1", Price = 100, CreateDate = DateTime.Now, Stock = 10 },
+                new() { Id = Guid.NewGuid(), Name = "Product2", Price = 200, CreateDate = DateTime.Now, Stock = 20 },
+                new() { Id = Guid.NewGuid(), Name = "Product3", Price = 300, CreateDate = DateTime.Now, Stock = 30 },
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
---
+++ İnfrastructrue/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+    using ETicaretAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using ETicaretAPI.Persistence.Repositories;
+using ETicaretAPI.Application.Repositories;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretDbContext>(options => options.UseNpgsql(Configration.ConnectionString));
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
